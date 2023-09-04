9138b8a581a83ba4c05a3ebd55be5ba7c0ce2601
El-Kiro <gapchuk1buba@gmail.com>
2023-09-04 23:12:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,20 @@ def get_birthdays_per_week(users):
 
     current_weekday = today.weekday()
 
-    start_of_week = today + datetime.timedelta(days=(7 - current_weekday))
+    start_of_week = today - datetime.timedelta(days=current_weekday)
     end_of_week = start_of_week + datetime.timedelta(days=6)
 
     birthdays_by_day = {i: [] for i in range(7)}
 
+
+
     for user in users:
-        birthday = user['birthday'].date()
+        birthday = user['birthday'].replace(year=today.year)
         if start_of_week <= birthday <= end_of_week:
             birthday_weekday = birthday.weekday()
-            birthdays_by_day[birthday_weekday].append(user['name'])
-
+            if birthday_weekday in (5, 6):
+                birthday += datetime.timedelta(days=(7-birthday_weekday))
+            birthdays_by_day[birthday.weekday()].append(user['name'])
     weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
     for i in range(7):
@@ -29,8 +32,8 @@ def get_birthdays_per_week(users):
 users = [
     {'name': 'Bill', 'birthday': datetime.date(2000, 9, 4)},
     {'name': 'Jill', 'birthday': datetime.date(2001, 9, 5)},
-    {'name': 'Kim', 'birthday': datetime.date(2002, 9, 8)},
-    {'name': 'Jan', 'birthday': datetime.date(2003, 9, 8)},
+    {'name': 'Kim', 'birthday': datetime.date(2002, 9, 9)},
+    {'name': 'Jan', 'birthday': datetime.date(2003, 9, 10)},
 ]
 
 get_birthdays_per_week(users)
